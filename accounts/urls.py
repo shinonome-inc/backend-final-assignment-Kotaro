7620ec755aded9f7639e6c58101746ea1d17c60f
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-11-14 00:22:01 +0900

    ログイン機能を作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
-# from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 
 from . import views
 
@@ -7,7 +7,10 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+            template_name="accounts/login.html",
+            next_page=reverse_lazy("tweets:home")
+        ), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path('<str:username>/', views.UserProfileView.as_view(), name='user_profile'),
     # path('<str:username>/follow/', views.FollowView.as_view(), name='follow'),
